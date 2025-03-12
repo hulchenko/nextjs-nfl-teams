30eb66c2b669ed65e490947f70be310ac202f577
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-03-12 17:44:18 -0300

    add authentication check of the X-API-KEY in headers

--- src/app/team_list/[league]/route.tsx
+++ src/app/team_list/[league]/route.tsx
@@ -1,24 +1,31 @@
-import { NextRequest, NextResponse } from "next/server";
 import { Leagues } from "@/app/interfaces/Leagues";
 import { QueryOptions } from "@/app/interfaces/QueryOptions";
+import { Team } from "@/app/interfaces/Team";
 import { sort } from "@/app/utils/format";
+import { NextRequest, NextResponse } from "next/server";
 
 import leagues from "@/app/data/leagues.json";
 import teams from "@/app/data/teams.json";
-import { Team } from "@/app/interfaces/Team";
+
+const VALID_API_KEY = process.env.API_KEY;
 
 const leagueNames = leagues.map((l) => l.name as Leagues);
-const sortParams = Object.values(QueryOptions);
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ league: Leagues }> }) {
+  // Security X-API-KEY check
+  const API_KEY = req.headers.get("X-API-KEY");
+  if (API_KEY !== VALID_API_KEY) {
+    return NextResponse.json({ error: "Unauthenticated" }, { status: 401 });
+  }
+
   // URL params
   const { league } = await params;
 
   // query
   const searchParams = req.nextUrl.searchParams;
   const incSortBy = searchParams.get("sort_by") as QueryOptions;
+  const sortParams = Object.values(QueryOptions);
   const sortBy = sortParams.includes(incSortBy) ? incSortBy : QueryOptions.Name; // if incoming param is empty/doesn't exist -> default to "name"
-  console.log("SORT BY: ", sortBy);
 
   // request validation
   if (!league) {
