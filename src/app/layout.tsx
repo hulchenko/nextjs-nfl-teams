ce056cb218638bd19181b5b94fd02f6d3ba7e243
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-03-13 00:50:11 -0300

    add swr, mui dependencies. fix nextjs hydration issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+// https://mui.com/material-ui/integrations/nextjs/?srsltid=AfmBOopLpjZeoTQZzkUaZ4T2evwKStsYk1ZiThkQh7lKUNmEjYnKsr10
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +29,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AppRouterCacheProvider>
+          <Header />
+          {children}
+          <Footer />
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
